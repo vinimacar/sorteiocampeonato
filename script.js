1ba99609ad8c09bd56174bf21ce0566ad1763133
vinimacar <132243328+vinimacar@users.noreply.github.com>
2025-07-04 15:06:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ window.onload = function() {
     div.ondragstart = drag;
     div.dataset.nome = sel.nome;
     div.dataset.codigo = sel.codigo;
-    div.innerHTML = `<img src="https://www.countryflags.com/wp-content/uploads/${sel.codigo}-flag-png-large.png" width="40" alt="${sel.nome}" class="me-2 rounded shadow-sm" onerror="this.onerror=null;this.src='https://www.countryflags.com/wp-content/uploads/un-flag-png-large.png';"> ${sel.nome}`;
+    div.innerHTML = `<img src="https://flagcdn.com/40x30/${sel.codigo}.png" width="40" alt="${sel.nome}" class="me-2 rounded shadow-sm" onerror="this.onerror=null;this.src='https://flagcdn.com/40x30/un.png';"> ${sel.nome}`;
     area.appendChild(div);
   });
 
@@ -90,7 +90,7 @@ function drop(ev) {
   div.draggable = true;
   div.dataset.nome = data.nome;
   div.dataset.codigo = data.codigo;
-  div.innerHTML = `<img src="https://www.countryflags.com/wp-content/uploads/${data.codigo}-flag-png-large.png" width="40" alt="${data.nome}" class="me-2 rounded shadow-sm" onerror="this.onerror=null;this.src='https://www.countryflags.com/wp-content/uploads/un-flag-png-large.png';"> ${data.nome}`;
+  div.innerHTML = `<img src="https://flagcdn.com/40x30/${data.codigo}.png" width="40" alt="${data.nome}" class="me-2 rounded shadow-sm" onerror="this.onerror=null;this.src='https://flagcdn.com/40x30/un.png';"> ${data.nome}`;
   div.ondragstart = drag;
   target.appendChild(div);
   target.classList.remove("dragover");
@@ -139,7 +139,7 @@ function mostrarGrupos() {
     col.className = "col-md-3";
     let html = `<div class="grupo"><h6>Grupo ${letras[idx]}</h6>`;
     grupo.forEach(sel => {
-      html += `<div><img src="https://www.countryflags.com/wp-content/uploads/${sel.codigo}-flag-png-large.png" width="40" alt="${sel.nome}" onerror="this.onerror=null;this.src='https://www.countryflags.com/wp-content/uploads/un-flag-png-large.png';"> ${sel.nome}</div>`;
+      html += `<div><img src="https://flagcdn.com/40x30/${sel.codigo}.png" width="40" alt="${sel.nome}" onerror="this.onerror=null;this.src='https://flagcdn.com/40x30/un.png';"> ${sel.nome}</div>`;
     });
     html += `</div>`;
     col.innerHTML = html;
@@ -165,8 +165,8 @@ function gerarTabela() {
     for (let i = 0; i < grupo.length; i++) {
       for (let j = i + 1; j < grupo.length; j++) {
         jogos.push(
-          `<img src="https://www.countryflags.com/wp-content/uploads/${grupo[i].codigo}-flag-png-large.png" width="40" alt="${grupo[i].nome}" onerror="this.onerror=null;this.src='https://www.countryflags.com/wp-content/uploads/un-flag-png-large.png';"> ${grupo[i].nome} x 
-           <img src="https://www.countryflags.com/wp-content/uploads/${grupo[j].codigo}-flag-png-large.png" width="40" alt="${grupo[j].nome}" onerror="this.onerror=null;this.src='https://www.countryflags.com/wp-content/uploads/un-flag-png-large.png';"> ${grupo[j].nome}`
+          `<img src="https://flagcdn.com/40x30/${grupo[i].codigo}.png" width="40" alt="${grupo[i].nome}" onerror="this.onerror=null;this.src='https://flagcdn.com/40x30/un.png';"> ${grupo[i].nome} x 
+           <img src="https://flagcdn.com/40x30/${grupo[j].codigo}.png" width="40" alt="${grupo[j].nome}" onerror="this.onerror=null;this.src='https://flagcdn.com/40x30/un.png';"> ${grupo[j].nome}`
         );
       }
     }
